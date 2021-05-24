0decfc33c4886eade8670d453713ae0913b7cf41
alexander-achilles <mrinmoym247@gmail.com>
2021-05-24 14:20:04 +0530

    Setup & manage API Key to get price

--- bin/coindex-check.js
+++ bin/coindex-check.js
@@ -4,8 +4,17 @@ const check = require('../commands/check');
 program
   .command('price')
   .description('Check price of coins')
-  .action(()=>{console.log("from check price");});
+  .option(
+    '--coin <type>',
+    'Add specific coin types in CSV format',
+    'BTC,ETH,XRP'
+  )
+  .option('--cur <currency>', 'Change the currency', 'USD')
+  .action(cmd => check.price(cmd));
 
 program.parse(process.argv);
 
-
+// If no args, output help
+if (!process.argv[2]) {
+  program.outputHelp();
+}
--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,20 +1,20 @@
 const program = require('commander');
-
+const key = require('../commands/key');
 
 program
   .command('set')
   .description('Set API Key -- Get at https://nomics.com')
-  .action(()=>{console.log("from set");});
+  .action(key.set);
 
 program
   .command('show')
   .description('Show API Key')
-  .action(()=>{console.log("from show");});
+  .action(key.show);
 
 program
   .command('remove')
   .description('Remove API Key')
-  .action(()=>{console.log("from remove");});
+  .action(key.remove);
 
 program.parse(process.argv);
 
